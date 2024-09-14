2265d86af66d2b1b2ea5e43cce17b7b2db1a7b96
lucas.henryd@utem.cl <noname@MacBook-Air-de-Lucas.local>
2024-09-14 02:13:13 -0300

    chore: more cool effects lol

--- lib/ui/quotation/widgets/time_wheel_picker.dart
+++ lib/ui/quotation/widgets/time_wheel_picker.dart
@@ -1,98 +1,137 @@
 import 'package:flutter/material.dart';
+import 'package:inker_studio/generated/l10n.dart';
+import 'package:inker_studio/ui/theme/text_style_theme.dart';
 
-class TimeWheelPicker extends StatefulWidget {
+class ImprovedTimeWheelPicker extends StatefulWidget {
   final TimeOfDay initialTime;
   final Function(TimeOfDay) onTimeSelected;
 
-  const TimeWheelPicker({
+  const ImprovedTimeWheelPicker({
     Key? key,
     required this.initialTime,
     required this.onTimeSelected,
   }) : super(key: key);
 
   @override
-  _TimeWheelPickerState createState() => _TimeWheelPickerState();
+  _ImprovedTimeWheelPickerState createState() =>
+      _ImprovedTimeWheelPickerState();
 }
 
-class _TimeWheelPickerState extends State<TimeWheelPicker> {
-  late int _selectedHour;
-  late int _selectedMinute;
+class _ImprovedTimeWheelPickerState extends State<ImprovedTimeWheelPicker> {
+  late FixedExtentScrollController _hourController;
+  late FixedExtentScrollController _minuteController;
+  late TimeOfDay _selectedTime;
 
   @override
   void initState() {
     super.initState();
-    _selectedHour = widget.initialTime.hour;
-    _selectedMinute = widget.initialTime.minute;
+    _selectedTime = widget.initialTime;
+    _hourController =
+        FixedExtentScrollController(initialItem: _selectedTime.hour);
+    _minuteController =
+        FixedExtentScrollController(initialItem: _selectedTime.minute);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 200,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          _buildWheel(
-            items:
-                List.generate(24, (index) => index.toString().padLeft(2, '0')),
-            initialItem: _selectedHour,
-            onChanged: (value) {
-              setState(() {
-                _selectedHour = value;
-                _updateSelectedTime();
-              });
-            },
-          ),
-          const SizedBox(width: 20),
-          _buildWheel(
-            items:
-                List.generate(60, (index) => index.toString().padLeft(2, '0')),
-            initialItem: _selectedMinute,
-            onChanged: (value) {
-              setState(() {
-                _selectedMinute = value;
-                _updateSelectedTime();
-              });
-            },
-          ),
-        ],
-      ),
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        final double wheelHeight = constraints.maxHeight * 0.7;
+        final double labelHeight = constraints.maxHeight * 0.15;
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _buildWheel(
+              label: S.of(context).hour,
+              controller: _hourController,
+              items: List.generate(
+                  24, (index) => index.toString().padLeft(2, '0')),
+              onChanged: (value) => _updateSelectedTime(),
+              wheelHeight: wheelHeight,
+              labelHeight: labelHeight,
+            ),
+            const SizedBox(width: 20),
+            _buildWheel(
+              label: S.of(context).minutes,
+              controller: _minuteController,
+              items: List.generate(
+                  60, (index) => index.toString().padLeft(2, '0')),
+              onChanged: (value) => _updateSelectedTime(),
+              wheelHeight: wheelHeight,
+              labelHeight: labelHeight,
+            ),
+          ],
+        );
+      },
     );
   }
 
   Widget _buildWheel({
+    required String label,
+    required FixedExtentScrollController controller,
     required List<String> items,
-    required int initialItem,
     required Function(int) onChanged,
+    required double wheelHeight,
+    required double labelHeight,
   }) {
-    return Container(
-      width: 70,
-      child: ListWheelScrollView.useDelegate(
-        itemExtent: 40,
-        perspective: 0.005,
-        diameterRatio: 1.2,
-        physics: const FixedExtentScrollPhysics(),
-        childDelegate: ListWheelChildBuilderDelegate(
-          childCount: items.length,
-          builder: (context, index) {
-            return Center(
-              child: Text(
-                items[index],
-                style: TextStyle(
-                  fontSize: 20,
-                  color: index == initialItem ? Colors.white : Colors.grey,
-                ),
-              ),
-            );
-          },
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        SizedBox(
+          height: labelHeight,
+          child: Center(
+            child: Text(
+              label,
+              style: TextStyleTheme.copyWith(color: Colors.white),
+            ),
+          ),
         ),
-        onSelectedItemChanged: onChanged,
-      ),
+        SizedBox(
+          height: wheelHeight,
+          width: 70,
+          child: ListWheelScrollView.useDelegate(
+            controller: controller,
+            itemExtent: 40,
+            perspective: 0.005,
+            diameterRatio: 1.2,
+            physics: const FixedExtentScrollPhysics(),
+            childDelegate: ListWheelChildBuilderDelegate(
+              childCount: items.length,
+              builder: (context, index) {
+                return Center(
+                  child: Text(
+                    items[index],
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: controller.selectedItem == index
+                          ? Colors.blue
+                          : Colors.grey,
+                    ),
+                  ),
+                );
+              },
+            ),
+            onSelectedItemChanged: onChanged,
+          ),
+        ),
+      ],
     );
   }
 
   void _updateSelectedTime() {
-    widget.onTimeSelected(
-        TimeOfDay(hour: _selectedHour, minute: _selectedMinute));
+    setState(() {
+      _selectedTime = TimeOfDay(
+        hour: _hourController.selectedItem,
+        minute: _minuteController.selectedItem,
+      );
+    });
+    widget.onTimeSelected(_selectedTime);
+  }
+
+  @override
+  void dispose() {
+    _hourController.dispose();
+    _minuteController.dispose();
+    super.dispose();
   }
 }
