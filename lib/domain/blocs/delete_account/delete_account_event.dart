c700602c8ba0d55c963ecfe8c2892c40a8b151b1
Lucas Henry <lucas.henryd@utem.cl>
2024-12-18 21:28:43 -0300

    feat: delete account, recover account, send emails notifciation with verification code

---
+++ lib/domain/blocs/delete_account/delete_account_event.dart
@@ -0,0 +1,7 @@
+
+part of 'delete_account_bloc.dart';
+
+@freezed
+class DeleteAccountEvent with _$DeleteAccountEvent {
+  const factory DeleteAccountEvent.requested(String password) = _DeleteAccountRequested;
+}
